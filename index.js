e450d69e507f413860873a4c3dd345c9b92585dc
walaa.alkasim <walaa.alkasim@hotmail.com>
2022-04-06 23:47:38 +0200

    oxfordDic

---
+++ index.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+//console.log(process.env);
+const axios = require("axios");
+
+const word = process.argv.slice(2);
+const app_key = process.env.APP_KEY;
+const app_id = process.env.APP_ID;
+axios
+  .get(
+    `https://od-api.oxforddictionaries.com/api/v2/entries/en/${word}?fields=definitions`,
+
+    {
+      // Accept: "application/json",
+      headers: {
+        app_id: app_id,
+        app_key: app_key,
+      },
+    }
+  )
+  .then(function (response) {
+    // let defItem;
+    const def = response.data.results[0].lexicalEntries[0].entries[0].senses;
+    def.map((item, index) => {
+      const defItem = item.definitions;
+      console.log(index + 1, ".", defItem.toString());
+    });
+    console.log("  ");
+    console.log("provided by", response.data.metadata.provider);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
