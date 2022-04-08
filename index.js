40b2f78a702ba9e2009293a079f88a0dd9c17ff0
James Newton <blackslate@lexogram.com>
2022-04-08 13:14:22 +0300

    Treatment of all lexical categories (first draft)
    * Create config variable for axios call
    * Create standalone treatResponse and treatError functions
    * Simplify call to axios, using config and standalone functions
    * Use [].reduce() to obtain all definitions from the whole hierarchy:
      + Lexical categories
        + Lexical entries
          + Entries
            + Senses
              + Definitions
    
    NOTE: treatError() not treated yet

--- index.js
+++ index.js
@@ -5,27 +5,85 @@ const axios = require("axios");
 const word = process.argv.slice(2); //use npm index.js happy///or any word you want
 const app_key = process.env.APP_KEY;
 const app_id = process.env.APP_ID;
-axios
-  .get(
-    `https://od-api.oxforddictionaries.com/api/v2/entries/en/${word}?fields=definitions`,
 
-    {
-      // Accept: "application/json",
-      headers: {
-        app_id: app_id,
-        app_key: app_key,
-      },
-    }
-  )
-  .then(function (response) {
-    const def = response.data.results[0].lexicalEntries[0].entries[0].senses;
-    def.map((item, index) => {
-      const defItem = item.definitions;
-      console.log(index + 1, ".", defItem.toString());
-    });
-    console.log("  ");
-    console.log("provided by", response.data.metadata.provider);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+
+const config = {
+  method: 'get',
+  url: `https://od-api.oxforddictionaries.com/api/v2/entries/en/${word}?fields=definitions`,
+  headers: {
+    app_id: app_id,
+    app_key: app_key,
+  }
+}
+
+
+const treatResponse = ({ data }) => {
+  const { metadata, results } = data
+
+  const usage = results.reduce(treatCategory, "")
+
+  console.log(`Definitions of "${word}", as provided by ${metadata.provider}`);
+  console.log(usage);
+
+}
+
+
+const treatCategory = (text, categoryData) => {
+  // console.log("**************\ncategoryData:", categoryData);
+
+  const { lexicalEntries } = categoryData
+
+  return text + lexicalEntries.reduce(treatLexicalEntry, "-----\n")
+}
+
+
+const treatLexicalEntry = ( text, lexicalEntry ) => {
+  // console.log("************\nlexicalEntry:", lexicalEntry);
+
+  const { entries, lexicalCategory } = lexicalEntry
+
+
+  return text
+      + lexicalCategory.text
+      + entries.reduce(treatEntry, "\n")
+
+}
+
+
+const treatEntry = ( text, entry ) => {
+  // console.log("**********\nentry:", entry);
+
+  const { senses } = entry
+
+  return text + senses.reduce(treatSense, "\n")
+
+}
+
+
+const treatSense = ( text, sense, index ) => {
+  // console.log("********\nsense:", sense);
+
+  const { definitions } = sense
+  const definition = (definitions.length === 1)
+  ? definitions[0]
+  : definitions.reduce(treatDefinition, "\n")
+
+  return `${text}${index + 1}. ${definition}\n`
+}
+
+
+const treatDefinition = ( text, definition, index ) => {
+  // console.log("*****\sdefinition:", definition);
+
+  return `${text}  ${index + 1}. ${definition}\n`
+}
+
+
+const treatError = (error) => {
+  console.log(error);
+}
+
+
+axios(config)
+  .then(treatResponse)
+  .catch(treatError);
