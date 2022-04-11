708918224d096ecb43df42a139542df661bb40e0
walaa.alkasim <walaa.alkasim@hotmail.com>
2022-04-11 23:57:37 +0200

    deal with word two parts

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 //console.log(process.env);
 const axios = require("axios");
 
-const word = process.argv.slice(2); //use npm index.js happy///or any word you want
+const word = process.argv.slice(2).join(" "); //use node index.js happy///or any word you want
 const app_key = process.env.APP_KEY;
 const app_id = process.env.APP_ID;
 axios
@@ -19,13 +19,27 @@ axios
   )
   .then(function (response) {
     const def = response.data.results[0].lexicalEntries[0].entries[0].senses;
+    console.log(
+      "definitions of",
+      word,
+      "provided by",
+      response.data.metadata.provider
+    );
     def.map((item, index) => {
       const defItem = item.definitions;
       console.log(index + 1, ".", defItem.toString());
     });
     console.log("  ");
-    console.log("provided by", response.data.metadata.provider);
   })
   .catch(function (error) {
-    console.log(error);
+    if (error.response.data) {
+      console.log(
+        "an error occured which is:",
+        error.response.data,
+        "status :",
+        error.response.status
+      );
+    } else {
+      console.log(error);
+    }
   });
